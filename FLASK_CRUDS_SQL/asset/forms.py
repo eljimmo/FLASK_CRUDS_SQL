bc73a5f042d371f02bb82e148c3b18914d0c2821
Jim <104743175+eljimmo@users.noreply.github.com>
2022-08-03 15:12:03 -0500

    Add files via upload
    still need to connect run.py with backend pages 
    
    html not loading up 
    
    
    connect db  to SQLite3

---
+++ FLASK_CRUDS_SQL/asset/forms.py
@@ -0,0 +1,50 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, HiddenField, SelectField, ValidationError
+from wtforms.validators import DataRequired
+from wtforms.fields.html5 import DateField
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+
+from asset.models import Asset
+
+
+def check_if_asset_exist(form, field):
+    asset = Asset.query.filter_by(asset_name=field.data).first()
+    if asset:
+        raise ValidationError('Asset Already Exists')
+
+
+def get_assets():
+    return Asset.query.all()
+
+
+class EditTransactionForm(FlaskForm):
+    type = StringField('Type', render_kw={'readonly': True})
+    asset_name = StringField('Asset Name', render_kw={'readonly': True})
+    person_name = StringField('Person', render_kw={'readonly': True})
+    start_time = DateField('Start Time', render_kw={'readonly': True})
+    # end_time = DateField('End Time', validators=[DataRequired()])
+    end_time = DateField('End Time', format='%Y-%m-%d', validators=[DataRequired()])
+    status = SelectField('Status', choices=[('In Use', 'In Use'), ('In Store', 'In Store')],
+                         validators=[DataRequired()])
+    submit = SubmitField('Update')
+
+
+class AddTransactionForm(FlaskForm):
+    type = SelectField('Type', validators=[DataRequired()],
+                       choices=[('harddisk', 'Hard Disk'), ('testingcard', 'Card')])
+    asset_name = QuerySelectField(label='Asset Name', validators=[DataRequired()], query_factory=get_assets,
+                                  allow_blank=True)
+    person_name = StringField('Person', validators=[DataRequired()])
+    start_time = DateField('Start Time', format='%Y-%m-%d', validators=[DataRequired()])
+    #start_time = DateField('Start Time', format='%m/%d/%y', render_kw={'placeholder': '6/20/15 for June 20, 2015'})
+    end_time = HiddenField('End Time')
+    status = SelectField('Status', validators=[DataRequired()], choices=[('In Use', 'In Use')])
+    submit = SubmitField('Add')
+
+
+class AddAssetForm(FlaskForm):
+    type = SelectField('Type', validators=[DataRequired()],
+                       choices=[('harddisk', 'Hard Disk'),
+                                ('testingcard', 'Card')])
+    asset_name = StringField('Asset Name', validators=[DataRequired(), check_if_asset_exist])
+    submit = SubmitField('Register')
---
+++ FLASK_CRUDS_SQL/auth/models.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from enum import unique
+from operator import index
+import bcrypt
+from flask_login import UserMixin
+from sqlalchemy import PrimaryKeyConstraint
+from run import *
+
+class User(UserMixin, db.Model):
+    __tablename__="users"
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(60))
+    user_email = db.Column(db.String(60), unique=True, index=True)
+    user_password = db.Column(db.String(80))
+    regristration_date = db.Column(db.DateTime, default=datetime.now)
+
+    #app/auth/models.py
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.user_password, password)
+
+    #class method belongs to a class but not associated to any class instance
+    @classmethod
+    def create_user(cls, user, email, password):
+
+        user = cls(user_name=user,
+                   user_email=email,
+                   user_password=bcrypt.generate_password_hash(password).decode('utf-8')
+                   )
+
+        db.session.add(user)
+        db.session.commit()
+
+        return
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
